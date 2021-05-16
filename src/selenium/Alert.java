463400eeca0137ab455739784e0b4fbea8c3af11
tipua <tipua@DESKTOP-JD0PQHI>
2021-05-16 19:39:00 -0400

    adding a change from local for merge

--- src/selenium/Alert.java
+++ src/selenium/Alert.java
@@ -8,6 +8,7 @@ public class Alert {
 	public static void main(String[] args) {
 	System.out.println();
 		System.out.println("Second change by somebody else");
+		System.out.println("Need to merge Changes from local");
 
 	}
 
